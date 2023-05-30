99c9a2c6a37f254f5f14382ffe2c0d5d6c9028d7
xyashino <szczypiorpan123@gmail.com>
2023-05-30 13:04:19 +0200

    Create base create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,30 @@
-export class CreateUserDto {}
+import { UserEntity } from '@types';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class CreateUserDto implements Partial<UserEntity> {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(8, 255)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  avatar: string;
+
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+}
