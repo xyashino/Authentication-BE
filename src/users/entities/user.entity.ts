9728ba0371aa831f75f8602ac6fe9b07320c8a6f
xyashino <szczypiorpan123@gmail.com>
2023-05-30 12:54:14 +0200

    Create base for user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  avatar: string;
+}
