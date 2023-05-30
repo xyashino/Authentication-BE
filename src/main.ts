70148b4aae983247a4f36e29d26de95d78731cac
xyashino <szczypiorpan123@gmail.com>
2023-05-30 12:45:38 +0200

    Add cookieParser to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from '@app.module';
 import { ConfigService } from '@nestjs/config';
 import { corsConfig } from '@config/cors.config';
 import { pipesConfig } from '@config/pipes.config';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors(corsConfig);
   app.useGlobalPipes(pipesConfig);
+  app.use(cookieParser());
   await app.listen(+configService.get('PORT'));
 }
 bootstrap();
