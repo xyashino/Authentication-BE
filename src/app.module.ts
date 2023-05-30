2526d81aeee8585a30f57c0a466f35b886888f9f
xyashino <szczypiorpan123@gmail.com>
2023-05-30 12:50:56 +0200

    Generate CRUD resource 'users' by NestJs CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
 })
 export class AppModule {}
