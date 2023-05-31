f5f4efc71e4a137a5a8d181a4365a135b00cca4b
xyashino <szczypiorpan123@gmail.com>
2023-05-31 17:17:20 +0200

    Add AuthModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfigAsync } from '@/config/typeorm.config';
+import { AuthModule } from './authentication/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeormConfigAsync),
+    AuthModule,
     UsersModule,
   ],
 })
