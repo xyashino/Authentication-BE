3b605411dfc293579c2c552a798b9e9c2ce834a9
xyashino <szczypiorpan123@gmail.com>
2023-06-02 17:43:36 +0200

    Add 'findOrCreateProviderUser' method in user.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Repository } from 'typeorm';
@@ -6,6 +11,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '@/users/entities/user.entity';
 import { hash } from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { UserEntity } from '@/types';
+
+type CreateProviderUser = Omit<
+  UserEntity,
+  'hashPwd' | 'currentTokenId' | 'phone' | 'id' | 'email'
+>;
 @Injectable()
 export class UsersService {
   @Inject(ConfigService)
@@ -18,7 +29,7 @@ export class UsersService {
     await this.checkEmail(email);
     const user = this.userRepository.create(restData);
     user.email = email;
-    user.password = await this.hashPassword(password);
+    user.hashPwd = await this.hashPassword(password);
     return this.userRepository.save(user);
   }
 
@@ -36,13 +47,24 @@ export class UsersService {
     const user = await this.findOne(id);
     if (restUser?.email) await this.checkEmail(restUser.email);
     this.userRepository.merge(user, restUser);
-    if (password) user.password = await this.hashPassword(password);
+    if (password) user.hashPwd = await this.hashPassword(password);
     return this.userRepository.save(user);
   }
 
   async remove(id: string) {
     const user = await this.findOne(id);
     return this.userRepository.remove(user);
+  }
+
+  async findOrCreateProviderUser(
+    email: string,
+    data: CreateProviderUser,
+  ): Promise<User> {
+    const foundUser = await this.userRepository.findOneBy({ email });
+    if (!foundUser) return await this.userRepository.save({ email, ...data });
+    if (foundUser.provider !== data.provider)
+      throw new ConflictException('You already have an account');
+    return foundUser;
   }
 
   private async checkEmail(email: string) {
